60c26ab3b2c0f1a6dd5d8824dbd9673f37d5838c
Jakub Lemiesiewicz <jlemiesiewicz@man.poznan.pl>
2022-06-03 00:20:59 +0200

    git

--- main.sh
+++ main.sh
@@ -1,48 +1,102 @@
 #!/bin/sh
 
-# getargs() {
-#     echo $@
-# }
 
 helpview() {
     echo "RAAAAA"
     echo "RRRRAAAAAAAAA"
+    echo "basz bi lajk"
 }
+
+
+lower() {
+    echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
 
 getwords() {
     words=""
     file="data.txt"
+    # file="/usr/share/dict/words"
     lines=`cat $file`
     for line in $lines; do
-        words="${words} $line"
+        words="${words} $(lower "$line")"
     done
     echo "$words"
 }
 
-main() {
-    args=$1
-    words=$(getwords)
-    # args=$(getargs)
 
-    echo "file"
-    for WORD in ${words}; do
+get_point_by_letter() {
+    echo 1
+}
+
+
+generate_result() {
+    correct=""
+    invalid=""
+    for user_word in ${user_words}; do
+        temp_checker=false
+        for aval_word in ${avaliable_words}; do
+            if [ "$user_word" = "$aval_word" ]; then
+                temp_checker=true
+            fi
+        done
+        if [ "$temp_checker" = true ] ; then
+            correct="${correct} $user_word"
+        else
+            invalid="${invalid} $user_word"
+        fi
+    done
+    points=0
+    echo "Correct words:"
+    for cw in ${correct}; do
+        for l in $(echo $cw | sed -e 's/\(.\)/\1\n/g'); do
+            points=$(( $points + $(get_point_by_letter "$l") ))
+        done
+    done
+    echo $points
+}
+
+
+with_args() {
+    for arg in ${args}; do
+        user_words="${user_words} $(lower "$arg")"
+    done
+}
+
+
+from_keyboard() {
+    read -p "Write numbers of words : " count
+    for i in $(seq 1 $count); do 
+        read -p "Write $i word " temp;
+        user_words="${user_words} $(lower "$temp")"
+    done
+}
+
+
+main() {
+    avaliable_words=$(getwords)
+    for WORD in ${avaliable_words}; do
         echo ${WORD};
     done
-
-    echo "args"
+    checker=false
     for arg in ${args}; do
-        echo ${arg};
+        checker=true
     done
-
+    if [ "$checker" = true ] ; then
+        with_args
+    else
+        from_keyboard
+    fi
+    generate_result
 }
 
 
 if [ "$1" != "-h" ]; then
-    main $@
+    args=""
+    for arg; do
+        args="${args} $(lower "$arg")"
+    done
+    main $args
 else
     helpview
 fi
-
-
-# $args=$@
-
